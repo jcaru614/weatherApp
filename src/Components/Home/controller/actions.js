32d5f3bb0f4a88adb0428d5f4d36c5cb096e7700
Joseph Caruana <josephcaruana@Josephs-MacBook-Pro.local>
2020-11-04 10:16:44 -0800

    text area

--- src/Components/Home/controller/actions.js
+++ src/Components/Home/controller/actions.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 
 const key = '';
 
-export const fetchWeatherRequest = (weather) => {
-    console.log("action ", weather)
+export const fetchWeatherRequest = (city) => {
+    console.log("action ", city)
     return dispatch => {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${weather}&appid=${key}`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
             .then(res => {
                 console.log("res ", res)
                 const response = res.data
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,33 +1,59 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchWeatherRequest } from './controller/actions';
-
+import mountains from '../../images/mountains.jpg';
+import "./styles.css";
+console.log('MOUNTAIN ', mountains)
 class Home extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            city: ''
+        }
     }
+
+    handleChange = (e) => {
+        this.setState({ city: e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.setWeather(this.state.city)
+        this.setState({
+            city: ''
+        })
+    }
+
     render() {
         return (
             <div>
-                <h1>Get the Weather</h1>
-                <button onClick={() => this.props.setWeather("Miami")}>Get Weather</button>
+            
+                <form onSubmit={this.handleSubmit}>
+                    <h1>Get the Weather</h1>
+                    <textarea onChange={this.handleChange} value={this.state.city} />
+                    <div>
+                        <button className='button'>Get Weather</button>
+                    </div>
+                </form>
                 <h2>number: {this.props?.weather?.main?.temp}</h2>
+                <image className='background' src={require(mountains)} alt="mountains" />
             </div>
         )
     }
 }
 
 function mapStateToProps(state) {
-    console.log("number mapstatetoprops ", state.homeReducer.weather)
+    console.log("mapstatetoprops ", state.homeReducer.weather)
     return {
         weather: state.homeReducer.weather
     }
 }
 
 function mapDispatchToProps(dispatch) {
+    console.log("mapdispatchtoprops ", dispatch)
     return {
-        setWeather: (weather) => {
-            dispatch(fetchWeatherRequest(weather))
+        setWeather: (city) => {
+            dispatch(fetchWeatherRequest(city))
         }
     };
 };
