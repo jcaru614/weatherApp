be1e7b34039e8797b89e54d255a7b46a41d4f720
Joseph Caruana <josephcaruana@Josephs-MacBook-Pro.local>
2020-11-11 11:01:10 -0800

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Home from './Components/Home/index';
 import { Provider } from "react-redux";
 import store from "./store"
 import Nav from '../src/Components/Nav/index'
@@ -8,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-          <Home />
           <Nav />
       </Provider>
     );
--- src/Components/Nav/index.tsx
+++ src/Components/Nav/index.tsx
@@ -1,34 +1,94 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Daily from '../Daily/index';
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
 
-const useStyles = makeStyles({
-    root: {
-      width: 500,
-    },
-  });
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
-export default function Nav() {
-    const classes = useStyles();
-    const [value, setValue] = useState(0);
-
-    return (
-        <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.root}
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
     >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      {/* <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} /> */}
-    </BottomNavigation>
-    )
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    width: 570,
+    position: 'absolute',
+    top: '10%',
+    left: '35.5%',
+    marginTop: '-50px',
+    marginLeft: '-50px',
+  },
+}));
+
+export default function FullWidthTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="secondary"
+            textColor="secondary"
+            variant="fullWidth"
+          >
+            <Tab label="Daily Weather" {...a11yProps(0)} />
+            <Tab label="Weekly Forecast" {...a11yProps(1)} />
+            <Tab label="Monthly Forecast" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
+      </div>
+      <TabPanel value={value} index={0} >
+        <Daily />
+        </TabPanel>
+      <TabPanel value={value} index={1} >
+        Item Two
+        </TabPanel>
+      <TabPanel value={value} index={2} >
+        Item Three
+        </TabPanel>
+
+
+    </>
+  );
 }
